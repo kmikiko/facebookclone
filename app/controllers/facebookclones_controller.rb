c7a47c6a9e61ff7b7ea5c383e078b306e070f6a7
Kanda Mikiko <kmikiko1121@gmail.com>
2023-05-26 17:09:44 +0900

    userモデルを作成

--- app/controllers/facebookclones_controller.rb
+++ app/controllers/facebookclones_controller.rb
@@ -1,5 +1,20 @@
 class FacebookclonesController < ApplicationController
   def index
-    
+    @facebookclones = Facebookclone.all
+  end
+
+  def new
+    @facebookclone = Facebookclone.new
+  end
+
+  def create
+    Facebookclone.create(facebookclone_params)
+    redirect_to new_facebookclone_path
+  end
+
+  private
+
+  def facebookclone_params
+    params.require(:facebookclone).permit(:content)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,9 +1,16 @@
-ActiveRecord::Schema.define(version: 2023_05_26_021243) do
-
+ActiveRecord::Schema.define(version: 2023_05_26_080258) do
   enable_extension "plpgsql"
 
   create_table "facebookclones", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
